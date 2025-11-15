057eb9d33e38674069c7fbef162ab9f00db94860
AnjaliPatel0 <anjalipatela78@gmail.com>
2025-11-15 12:00:33 +0530

    Contest Sheet

---
+++ Contest Sheet/Adding_Bits.py
@@ -0,0 +1,5 @@
+#F.Adding Bits
+A,B=map(int,input().split())
+result =  A^ B 
+print(result)
+
--- Contest Sheet/Interval_Sweep.py
+++ Contest Sheet/Interval_Sweep.py
@@ -1,1 +1,6 @@
 #E. Interval Sweep
+a,b=map(int,input().split())
+if a-b==1 or a-b==-1:
+    print("YES")
+else:
+    print("NO")    
