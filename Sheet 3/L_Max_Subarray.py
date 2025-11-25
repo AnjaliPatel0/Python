daa219351fc34bde8d23f64a9b9f35b1e3079492
AnjaliPatel0 <anjalipatela78@gmail.com>
2025-11-25 15:20:54 +0530

    sheet 3

--- Sheet 3/L_Max_Subarray.py
+++ Sheet 3/L_Max_Subarray.py
@@ -1,11 +1,16 @@
 #L. Max Subarray
-t=int(input())
-for _ in range(t):
-    n=int(input())
-    arr=list(map(int,input().split()))
-    subarray=[]
-    for i in range(n):
-        for j in range(i,n):
-            subarray.append(arr[i:j+1])
 
-        print(subarray)    
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    nums = input()
+    nums = list(map(int, nums.split()))
+    for i in range(len(nums)):
+        for j in range(0,i+1):
+            maxEle = nums[j]
+            idx = j
+            while(idx <= i):
+                maxEle = max(maxEle,nums[idx])
+                idx+=1
+            print(maxEle,end=" ")
+    print()
--- Sheet 3/N_Check_code.py
+++ Sheet 3/N_Check_code.py
@@ -1,8 +1,12 @@
 # N. Check Code
-a,b=map(int,input().split())
-s=input()
-
-if len(s)!=a+b+1:
-    print("No")
+a,b = map(int, input().split())
+s = input()
+for i in range(len(s)):
+    if i == a and s[i] != "-":
+        print("No")
+        break
+    elif i != a and s[i].isdigit() == False:
+        print("No")
+        break
 else:
-         
+    print("Yes")
