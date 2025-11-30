e51248c2ae6c284c7be2f6e1b979bcc8c8465ce7
AnjaliPatel0 <anjalipatela78@gmail.com>
2025-11-30 06:53:08 +0530

    shee3

--- Sheet 3/U_Is_B_a_subsequence_of_A.py
+++ Sheet 3/U_Is_B_a_subsequence_of_A.py
@@ -1,8 +1,19 @@
 #U. Is B a subsequence of A ?
-n,m=map(int,input().split())
 
-
-a=int(input())
-
-b=int(input())    
-    
+n,m = map(int, input().split())
+a = input()
+b = input()
+a = list(map(int, a.split()))
+b = list(map(int, b.split()))
+idx1 = 0
+idx2 = 0
+while idx1 < len(a) and idx2 < len(b):
+    if a[idx1] == b[idx2]:
+        idx1 += 1
+        idx2 += 1
+    else:
+        idx1 += 1
+if idx2 == len(b):
+    print("YES")
+else:
+    print("NO")
