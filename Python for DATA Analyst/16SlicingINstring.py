80f51d22a3adbfea16bcc2e5f90116d99d55ecbb
AnjaliPatel0 <anjalipatela78@gmail.com>
2026-08-18 11:26:21 +0530

    String Slicing

---
+++ Python for DATA Analyst/16SlicingINstring.py
@@ -0,0 +1,12 @@
+#String Slicing
+a="Harry potter and te Goblet of fire"
+b="0123456789"
+print(a)
+print(b)
+print(b[::2])
+print(b[:7:2])
+print(b[::-1])
+print(a[0:5])
+print(a[:5])
+print(a[-4:])
+print(a[6:12])
